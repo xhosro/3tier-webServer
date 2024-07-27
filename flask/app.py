b5fa32fd70c9315cda6664d97d6ffd9d6059efb3
xhosro <khosro.hengaam@gmail.com>
2024-07-27 04:19:51 +0200

    creat a flask app for logic tier and test it

---
+++ flask/app.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/about', methods=['GET'])
+def about():
+    version = os.environ.get('APP_VERSION')
+
+    return { 'version': version}, 200
+
